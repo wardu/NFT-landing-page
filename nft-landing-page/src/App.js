62f13d5f2b8b43ae5a62f161b76786dd3ba568c2
Wardu <wardu@wardu.home>
2022-07-08 17:35:54 +0100

    Add react framework and COMING SOON

---
+++ nft-landing-page/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Coming Soon</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
